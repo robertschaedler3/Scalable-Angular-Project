0b33c66b6dac061b1519a5e78561bc2b4b7dc783
Robert Schaedler III <robertschaedler3@gmail.com>
2021-01-21 22:28:29 -0500

    create footer component

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -5,6 +5,7 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 
 // Components
+import { FooterComponent } from './components/footer/footer.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 // Pages
@@ -12,7 +13,12 @@ import { MainPageComponent } from './pages/main-page/main-page.component';
 
 
 @NgModule({
-  declarations: [MainComponent, ToolbarComponent, MainPageComponent],
+  declarations: [
+    MainComponent,
+    ToolbarComponent,
+    MainPageComponent,
+    FooterComponent,
+  ],
   imports: [CommonModule, MainRoutingModule],
 })
 export class MainModule {}
